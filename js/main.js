0392e1422264f18a9e8d2c2c814b2a4e99b1e885
nestaleks <nestaleks83@gmail.com>
2024-02-28 23:19:51 +0200

    1

--- js/main.js
+++ js/main.js
@@ -19,16 +19,3 @@ $('.menu__btn').on('click', function () {
 	$('.menu__list').removeClass('menu--open');
   });
 
-
-
-  console.log(Instafeed)
-  var feed = new Instafeed({
-    accessToken: InstagramToken,
-    transform: function(item) {
-      var d = new Date(item.timestamp);
-      item.date = [d.getDate(), d.getMonth(), d.getYear()].join('/');
-      return item;
-    }
-  });
-  
-  feed.run();
