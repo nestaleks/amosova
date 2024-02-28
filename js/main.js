b3553025797f13fd86c45bef279934fad28c5ec5
nestaleks <nestaleks83@gmail.com>
2024-02-28 23:39:11 +0200

    1

--- js/main.js
+++ js/main.js
@@ -19,3 +19,20 @@ $('.menu__btn').on('click', function () {
 	$('.menu__list').removeClass('menu--open');
   });
 
+
+  function onEntry(entry) { 
+    entry.forEach(change => { 
+      if (change.isIntersecting) { 
+       change.target.classList.add('element-show'); 
+       
+      } 
+    }); 
+  } 
+      let options = { 
+    threshold: [0.5] }; 
+  let observer = new IntersectionObserver(onEntry, options); 
+  let elements = document.querySelectorAll('.services__item-img'); 
+   
+  for (let elm of elements) { 
+    observer.observe(elm); 
+  }
